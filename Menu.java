a219d219c4796a63f52eb0cad00e00a5216ecd19
unknown <quang24101977@gmail.com>
2024-12-06 22:40:16 +0700

    create menu

--- Menu.java
+++ Menu.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class menu {
+public class Menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("-----Library Management-----");
+            System.out.println("1. Add name student ");
+            System.out.println("2. Delete name student");
+            System.out.println("3. Show name student.");
+            System.out.println("4. Exit program");
+            System.out.println("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("Exiting program. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+
+        }while (choice!=4);
+    }
 }
+
